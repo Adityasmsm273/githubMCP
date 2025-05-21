c14e6141ab0001f5883f46eb6cb3000fe534e649
Anushacharyaa <anushacharyaa@gmail.com>
2025-05-21 16:17:26 +0530

    commit2

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
-def add(a,b):
-    return a-b
+def addition(num1, num2):
+    """
+    Adds two numbers together.
+    
+    Args:
+        num1 (int/float): First number to add
+        num2 (int/float): Second number to add
+        
+    Returns:
+        int/float: The sum of num1 and num2
+    """
+    return num1 + num2
+
